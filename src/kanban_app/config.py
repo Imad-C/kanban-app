82f74af7487943dfa6a319fb26597576199fd41c
Imad <imadchowdhury01@gmail.com>
2024-07-22 21:33:10 +0100

    fixed sql db path

--- src/kanban_app/config.py
+++ src/kanban_app/config.py
@@ -5,15 +5,17 @@ from pathlib import Path
 config = ConfigParser(allow_no_value=True)
 
 def config_init() -> None:
-    project_dir = Path(__file__).parent
-    config['PATHS'] = {
-        "db": project_dir / "/data/boards.db",
-        "sql": f"sqlite:///{project_dir}/data/boards.db"
-    }
+    # project_dir = Path(__file__).parent
     # config['PATHS'] = {
-    #     "db": "src/kanban_app/data/boards.db",
-    #     "sql": "sqlite:///src/kanban_app/data/boards.db"
+    #     "db": project_dir / "/data/boards.db",
+    #     "sql": f"sqlite:///{project_dir}/data/boards.db"
     # }
+    # above doesn't work once cloned from GitHub, need to work out why
+    # below has problem where app only works if in project directory
+    config['PATHS'] = {
+        "db": "src/kanban_app/data/boards.db",
+        "sql": "sqlite:///src/kanban_app/data/boards.db"
+    }
 
     config['BOARDS'] = {
         "default": ["Not Started", "In Progress", "Completed"],
